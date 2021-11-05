b04af591f5e90b3f809e3b89b310b50e8e93a069
Huang-Bo <pioneer0221@outlook.com>
2021-11-05 11:56:52 -0700

    MiniCity finished by HuangBo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-//import {ThreeCanvas} from "./Components/Three"
-import {EThreeCanvas} from './Components/Three'
+import {ThreeCanvas} from "./Components/Three"
 
 function App() {
   
   return (
     <div className="App">
       <div className="title">Night of MiniCity</div>
-      <div className="maker">By Sutomo</div>
-      <EThreeCanvas/>
+      <div className="maker">By HuangBo</div>
+      <ThreeCanvas/>
     </div>
   );
 }
